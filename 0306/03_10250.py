9aca040ac3950f4c32d45757eb8d316014d85501
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-06 11:50:53 +0900

    호텔 : N이 H의 배수인 경우를 보정

--- 0306/03_10250.py
+++ 0306/03_10250.py
@@ -5,17 +5,15 @@
 '''
 
 def reserve(H,W,N): # H, W, N 에 대한 방 번호 반환
-    # 1, 1, 1 이면 101
-    # 11, 1, 1 이면 1101 -> H에 대해 100을 곱하고 숫자에 따라
-    # N에서 H로 나머지 연산을 통해 번호를 구한다
-    # H가 6이면 N이 10일 때, 10번재 사람은 4층 (402호)에 입주하게 된다
-
     ''' 이중 for문이 아니라 나머지, 몫 연산을 통해 방 번호를 구할 수 있지 않나? '''
+    # H가 6이고 N이 6일 때, 601호에 입주하게된다.
+    # (N이 H의 배수인 경우 보정 필요)
 
     floor = N % H
-    room_num = N // H + 1
-    room = floor*100 + room_num
-    return room
+    if floor == 0:
+        floor = H  # 0층이 아닌 최상층으로 변경
+    room_num = (N - 1) // H + 1  # N이 H의 배수일 때 보정
+    return floor * 100 + room_num
 
 
 test_count = int(input())
