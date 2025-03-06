af1d3b3e8593972a5d1c216ab5ba38ca934fc356
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-06 11:33:12 +0900

    이중 for문이 아닌 방식
    틀림. 나머지 연산이 아닌가?
    
    만약 N이 9이고, H가 3이면, floor는 0이됨. 3이 되어야 함.

--- 0306/03_10250.py
+++ 0306/03_10250.py
@@ -2,7 +2,6 @@
 
 백준 10250 문제, ACM 호텔
 
-
 '''
 
 def reserve(H,W,N): # H, W, N 에 대한 방 번호 반환
@@ -10,13 +9,14 @@ def reserve(H,W,N): # H, W, N 에 대한 방 번호 반환
     # 11, 1, 1 이면 1101 -> H에 대해 100을 곱하고 숫자에 따라
     # N에서 H로 나머지 연산을 통해 번호를 구한다
     # H가 6이면 N이 10일 때, 10번재 사람은 4층 (402호)에 입주하게 된다
-    count = 1
-    for room in range(1, W+1): # room = 1,2,3, ..., W
-        for floor in range(1, H+1): # floor = 1,2,3, ..., H
-            if count == N:
-                return floor * 100 + room
-            count += 1
-    pass
+
+    ''' 이중 for문이 아니라 나머지, 몫 연산을 통해 방 번호를 구할 수 있지 않나? '''
+
+    floor = N % H
+    room_num = N // H + 1
+    room = floor*100 + room_num
+    return room
+
 
 test_count = int(input())
 for _ in range(test_count):
