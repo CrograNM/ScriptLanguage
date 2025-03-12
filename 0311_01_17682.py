2a2e8a20ecd0a5285c391e6558a2e0bf5f6852a2
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-12 09:34:02 +0900

    0312 수업

--- 0311_01_17682.py
+++ 0311_01_17682.py
@@ -9,11 +9,11 @@ def solution(dartResult):
     score = []
     for i in dartResult:
         if i.isnumeric():
-            n += i
+            n += i # 숫자를 저장
         elif i == 'S':
             n = int(n) ** 1
             score.append(n)
-            n = ''
+            n = '' # 재 초기화
         elif i == 'D':
             n = int(n) ** 2
             score.append(n)
