3cf7d6b9a1e6492ad7d227fa067b5bf35a3c7ec3
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-27 11:06:36 +0900

    백준 1991 - 트리순회(1)

---
+++ 4주차/0327_04_1991.py
@@ -0,0 +1,21 @@
+"""
+    백준 1991 - 트리 순회
+"""
+
+def preorder():
+    print()
+    preorder()
+    preorder()
+def inorder():
+    inorder()
+    print()
+    inorder()
+def postorder():
+    postorder()
+    postorder()
+    print()
+
+N = int(input())
+print(N)
+M = {'A':('B','C')}
+print(M)
