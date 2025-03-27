a79520834979ebe516a5c1fdaff4ca750b0ea329
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-03-27 10:43:02 +0900

    재귀함수

---
+++ 4주차/0327_03.py
@@ -0,0 +1,17 @@
+""" 재귀 함수 """
+
+""" 무한한 재귀 호출 : 재귀 깊이 초과 오류가 나며 종료 된다 """
+# def recursive_func():
+#     print("재귀 호출")
+#     recursive_func()
+
+""" 재귀 함수의 종료 조건을 잘 설정해 주어야 한다. """
+# def recursive_func(i):
+#     if i == 100:
+#         return
+#     print(i,"번째 함수에서", i+1, "번째 재귀 함수 호출")
+#     recursive_func(i+1)
+#     print(i, "번째 재귀 함수를 종료합니다")
+# recursive_func(1)
+
+""" 팩토리얼 구현 예제 """
