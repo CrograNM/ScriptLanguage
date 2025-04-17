65c0e50f04345fd65b897cbf5ffa9614e7684478
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-04-17 10:05:23 +0900

    TUK 0063 : 행렬게임

--- 7주차/0416_07_0063.py
+++ 7주차/0416_07_0063.py
@@ -14,41 +14,69 @@
         T, 
             M의 전치행렬 
 """
+
+def print2DList(list):
+    for i in range( len(list) ):
+        for j in range( len(list[0]) ):
+            if j == len(list[0]) - 1:
+                print(list[i][j])
+            else:
+                print(list[i][j], end=' ')
+
+A,B = map(int, input().split())
+
 """
 M
 1 2 3 4 5
 6 7 8 9 10
 11 12 13 14 15
+"""
+print('M')
+M = [[ r*B + c + 1 for c in range(B)] for r in range(A) ]
+print2DList(M)
+
+"""
 R
-11 6 1
-12 7 2
-13 8 3
-14 9 4
-15 10 5
+11 6 1          00 01 02
+12 7 2          10 11 12
+13 8 3          20 21 22
+14 9 4          30 31 32
+15 10 5         40 41 42
+"""
+print('R')
+R = [[ M[r][c] for r in range(A-1, -1, -1) ] for c in range(B)]   # 위에서 B에 해당하는 원소를 c로 썼으니 똑같이 한다
+print2DList(R)
+# num = 1
+# R = [[0]*A for _ in range(B)]
+# for i in range(1, A+1):      # 3
+#     for j in range(B):  # 5
+#         R[j][-i] = num
+#         num+=1
+# print2DList(list)
+
+"""
 L
 5 10 15         00 01 02
 4 9 14          10 11 12
 3 8 13          20 21 22
 2 7 12          30 31 32
 1 6 11          40 41 42
-T
-1 6 11
-2 7 12
-3 8 13
-4 9 14
-5 10 15
 """
-
-A,B = map(int, input().split())
-
-print('M')
+print('L')
 num = 1
-for i in range(A):
-    for j in range(B):
-        print(num, end=' ')
-        num += 1
-    print()
-print('R')
+L = [[0]*A for _ in range(B)]
+#print(list)
+for i in range(A):           # 3
+    for j in range(1, B+1):  # 5
+        L[-j][i] = num
+        num+=1
+print2DList(L)
 
-list = [[0]*A for _ in range(B)]
-print(list)
+"""
+T
+1 6 11          00 01 02
+2 7 12          10 11 12
+3 8 13          20 21 22
+4 9 14          30 31 32
+5 10 15         40 41 42
+"""
