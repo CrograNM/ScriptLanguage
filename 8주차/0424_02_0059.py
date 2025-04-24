b8f1d82ab1d94f345567515bd9ee2383e663999a
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-04-24 10:16:07 +0900

    TUK 0059 - Profix 표현식

---
+++ 8주차/0424_02_0059.py
@@ -0,0 +1,28 @@
+"""
+    TUK 0059 - Postfix 표현식
+"""
+from collections import deque
+
+"""
+    ab+cd+e-a+-
+    
+    스택을 활용해서 구현
+    1. 문자가 연산자가 아니면 스택에 삽입
+    2. 문자가 연산자가 아니면 스택에 삽입
+    3. 연산자면 1,2를 pop, 괄호로 묶어 스택 삽입
+"""
+
+postfix = input()
+stack = deque()
+
+for c in postfix:
+    if c.isalpha():
+        stack.append(c)
+
+    elif c == '+' or c == '-':
+        right = stack.pop()
+        left = stack.pop()
+        bundle = '(' + left + c + right + ')'
+        stack.append(bundle)
+
+print(stack.pop())
