bf588374efe7fb85b7c30487a78bb3c71a5c3e7b
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-05-07 10:59:20 +0900

    프로그래머스 - 등굣길

--- 10주차-다이나믹/0507_03_WayToSchool.py
+++ 10주차-다이나믹/0507_03_WayToSchool.py
@@ -3,38 +3,30 @@
 """
 
 def solution(m, n, puddles):
-    # nxm 행렬
-    # DP 테이블 사용.
-    # 점화식, dp[i][j] = 여기까지 오는 최단 경로의 수
-    # dp[i][0] = 1, dp[0][j] = 1, dp[1][1] = 0
-    # dp[i][j] = dp[i-1][j] + dp[i][j-1] if not puddles
+    dp = [[0]*(m) for _ in range(n)]
+    puddleList = [[0]*m for _ in range(n)]
+    for [c,r] in puddles:
+        puddleList[r-1][c-1] = 1
 
-    # 일단 n*m으로 하고, 나중에 dp[m][n]을 출력
-    dp = [[0]*(m+1) for _ in range(n+1)]
-    for i in range(1, n+1):
-        dp[i][1] = 1
-    for j in range(1, m+1):
-        dp[1][j] = 1
-    for p in puddles:
-        # p = [2,2] 같은 리스트임
-        # p[0], p[1]을 통해 물 웅덩이의 좌표를 구함
-        dp[p[1]][p[0]] = -1
+    dp[0][0] = 1
+    # for i in range(n):
+    #     dp[0][i] = 1
+    # for j in range(n):
+    #     dp[j][0] = 1
 
-    for i in range(2, n+1):
-        for j in range(2, m+1):
-            if dp[i][j] != -1:
-                a = dp[i - 1][j]
-                if a == -1:
-                    a = 0
-                b = dp[i][j - 1]
-                if b == -1:
-                    b = 0
-                dp[i][j] = a + b
-
+    for r in range(n):
+        for c in range(m):
+            if puddleList[r][c] == 1:
+                dp[r][c] = 0
+            else:
+                if r > 0:
+                    dp[r][c] += dp[r-1][c]
+                if c > 0:
+                    dp[r][c] += dp[r][c-1]
     # 출력
-    # for i in range(0, n + 1):
-    #     print(dp[i])
-    answer = dp[n][m] % 1000000007
+    for i in range(n):
+        print(dp[i])
+    answer = dp[n-1][m-1] % 1000000007
     return answer
 
 print(solution(4, 3, [[2,2]]))
