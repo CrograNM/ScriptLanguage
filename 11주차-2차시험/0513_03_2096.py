85a5b1a400ea606f9529d6bae7e7465d2c316e00
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-05-13 23:53:34 +0900

    백준 2096 - 내려가기

--- 11주차-2차시험/0513_03_2096.py
+++ 11주차-2차시험/0513_03_2096.py
@@ -1,69 +1,31 @@
-"""
-    백준 2096 - 내려가기
-"""
-"""
-    DP? - 각 항에 대한 점화식을 만들자
-    1 2 3
-    1번 자리에서 아래로 내려갈 때, 3번으로 이동 불가
-    2번 자리에서 아래로 내려갈 때, 모두 이동 가능
-    3번 자리에서 아래로 내려갈 때, 1번으로 이동 불가
-"""
-"""
-    dp[i][j] = i레벨 j번째 항까지의 최대값
-    
-    dp[i][j] = array[i][j] + max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) 
-        if j = 0 : [i-1][0], [i-1][1] 만 max에 포함
-        if j = 1 : max에 세개 모두 가능
-        if j = 2 : [i-1][1], [i-1][2] 만 max에 포함
-"""
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-array = list()
-for i in range(N):
-    array.append(list(map(int, input().split())))
+prev_max = [0] * 3
+prev_min = [0] * 3
 
-dp = [[0]*3 for _ in range(N)]
-#print(dp)
+a, b, c = map(int, input().split())
+prev_max = [a, b, c]
+prev_min = [a, b, c]
 
-# max 구하기
-for i in range(N):
-    for j in range(3):
-        if i > 0:
-            max_list = list()
-            if j == 0:
-                max_list.append(dp[i - 1][0])
-                max_list.append(dp[i - 1][1])
-            elif j == 1:
-                max_list.append(dp[i - 1][0])
-                max_list.append(dp[i - 1][1])
-                max_list.append(dp[i - 1][2])
-            elif j == 2:
-                max_list.append(dp[i - 1][1])
-                max_list.append(dp[i - 1][2])
-            dp[i][j] = array[i][j] + max(max_list)
-        else:
-            dp[i][j] = array[i][j]
-MAX = max(dp[N-1])
+for _ in range(N - 1):
+    a, b, c = map(int, input().split())
 
-# min 구하기
-for i in range(N):
-    for j in range(3):
-        if i > 0:
-            min_list = list()
-            if j == 0:
-                min_list.append(dp[i - 1][0])
-                min_list.append(dp[i - 1][1])
-            elif j == 1:
-                min_list.append(dp[i - 1][0])
-                min_list.append(dp[i - 1][1])
-                min_list.append(dp[i - 1][2])
-            elif j == 2:
-                min_list.append(dp[i - 1][1])
-                min_list.append(dp[i - 1][2])
-            dp[i][j] = array[i][j] + min(min_list)
-        else:
-            dp[i][j] = array[i][j]
-MIN = min(dp[N-1])
+    # 이전 상태만을 참조해서 새로운 상태 계산
+    curr_max = [
+        a + max(prev_max[0], prev_max[1]),
+        b + max(prev_max[0], prev_max[1], prev_max[2]),
+        c + max(prev_max[1], prev_max[2])
+    ]
+    curr_min = [
+        a + min(prev_min[0], prev_min[1]),
+        b + min(prev_min[0], prev_min[1], prev_min[2]),
+        c + min(prev_min[1], prev_min[2])
+    ]
 
-print(MAX, MIN)
+    # 현재 결과를 이전으로 덮어씀
+    prev_max = curr_max
+    prev_min = curr_min
 
+print(max(prev_max), min(prev_min))
